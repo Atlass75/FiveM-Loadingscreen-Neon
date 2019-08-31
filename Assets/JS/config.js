e5117e7fd8a6ba959c2188b27cb877b5a40e820f
Atlass75 <p.atlass75@gmail.com>
2019-08-31 10:52:15 +0200

    Initial commit

---
+++ Assets/JS/config.js
@@ -0,0 +1,21 @@
+var video_ID = "UdB4ytHvX8o" // ID de la video Youtube
+var mp3_player = true // Si true, le lecteur MP3 sera utilisé et le son de vidéo youtube coupé
+
+var start = 22 // Permets de définir à partir de combien de secondes la vidéo Youtube doit commencer
+
+var img = "Logo.jpg" // Le nom de fichier du logo de votre serveur (* Il doit se situer dans le dossier : Assets/IMG *)
+
+var Discord_link = "https://discord.gg/4tpVfDv" // Lien de vôtre server Discord
+
+var red_text = 'California' // Texte en rouge
+var blue_text = 'You know ?' // Texte en bleu
+
+// N'oubliez d'ajouter vos musiques dans le __resource.lua
+// Placez vos musiques dans le dossier : Assets/AUIDO
+var tracks
+tracks = [{
+	"track": 1, // Position dans la liste
+	"name": "Migos - Get Right Witcha", // Le nom qui va apparête
+	"duration": "",
+	"file": "Migos_Get_Right_Witcha_Official_Video" // Le nom du fichier MP3 (Il ne doit pas contenir d'espace ou de caractère spéciaux.
+}]
---
+++ __resource.lua
@@ -0,0 +1,23 @@
+description 'Ecran de chargement'
+
+files {
+	'Index.html',
+
+	'Assets/Audio/Migos_Get_Right_Witcha_Official_Video.mp3',
+
+	'Assets/CSS/Neon.css',
+	'Assets/CSS/Styles.css',
+	'Assets/CSS/Player.css',
+
+	'Assets/IMG/cursor.png',
+	'Assets/IMG/X.png',
+	'Assets/IMG/Music.png',
+	'Assets/IMG/Logo.png',
+	
+	'Assets/JS/Index.js',
+	'Assets/JS/player.js',
+	'Assets/JS/progressBar.js',
+	'Assets/JS/config.js'
+}
+
+loadscreen 'Index.html'
